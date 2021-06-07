8f0fa3ff76f5a485c5aaccde63aac9c6025cce77
Troy Grossi <ttroygrossit@gmail.com>
2021-06-06 18:34:52 -0700

    changable times, color coded progress box, dynamic display

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-var date = moment().format("MMM Do YY");
+var date = moment().format("MMMM Do YYYY");
 var time = moment().format("h:mm:ss a");
 console.log(date);
 console.log(time);
@@ -8,4 +8,43 @@ var getDate = function () {
   $("#time").text(time);
 };
 
+var timeStatus = function () {};
+
+$(".check-box").click(function () {
+  var index = $(this).attr("data-progress-index");
+  console.log($("[data-progress-index='" + index + "']"));
+  if (!$(this)[0].innerText) {
+    $(this)[0].innerText = "In Progress";
+    $(this)[0].style.background = getComputedStyle(
+      document.documentElement
+    ).getPropertyValue("--color-yellow");
+  } else if ($(this)[0].innerText === "In Progress") {
+    $(this)[0].innerText = "Complete";
+    $(this)[0].style.background = getComputedStyle(
+      document.documentElement
+    ).getPropertyValue("--color-green");
+    console.log($("[data-progress-index='" + index + "']"));
+    console.log($(this)[0].backgroundColor);
+  } else if ($(this)[0].innerText === "Complete") {
+    $(this)[0].innerText = "";
+    $(this)[0].style.background = getComputedStyle(
+      document.documentElement
+    ).getPropertyValue("--color-blank");
+  } else {
+    $(this)[0].innerText = "In Progress";
+    $(this)[0].style.background = getComputedStyle(
+      document.documentElement
+    ).getPropertyValue("--color-yellow");
+  }
+  console.log($(this)[0].backgroundColor);
+});
+
 getDate();
+var count = 0;
+timeStatus();
+$("[data-index]").each(function () {
+  count++;
+  console.log($(this).attr("data-index"));
+});
+
+// checkTime();

2aca97b872ee6d9f877b0de642239a7c6c589064
Troy Grossi <ttroygrossit@gmail.com>
2021-06-06 20:57:34 -0700

    added local storage save content

--- assets/script.js
+++ assets/script.js
@@ -10,6 +10,7 @@ var getDate = function () {
 
 var timeStatus = function () {};
 
+//Changes progress box on click; chnages color and text value
 $(".check-box").click(function () {
   var index = $(this).attr("data-progress-index");
   console.log($("[data-progress-index='" + index + "']"));
@@ -39,12 +40,75 @@ $(".check-box").click(function () {
   console.log($(this)[0].backgroundColor);
 });
 
+var saveLocalData = function () {
+  localStorage.clear;
+  var index = 1;
+  $("[data-hour-index]").each(function () {
+    localStorage.setItem("hour" + index, $(this).val());
+    index++;
+  });
+  index = 1;
+  $("[data-minute-index]").each(function () {
+    localStorage.setItem("minute" + index, $(this).val());
+    index++;
+  });
+  index = 1;
+  $("[data-am-pm-index]").each(function () {
+    localStorage.setItem("am-pm" + index, $(this).val());
+    index++;
+  });
+  index = 1;
+  $("[data-text-index]").each(function () {
+    localStorage.setItem("text" + index, $(this).val());
+    index++;
+  });
+  index = 1;
+  $("[data-progress-index]").each(function () {
+    localStorage.setItem("progress" + index, $(this).text());
+    index++;
+  });
+};
+var getLocalData = function () {
+  console.log("get data");
+  var index = 1;
+  $("[data-hour-index]").each(function () {
+    $(this).val(localStorage.getItem("hour" + index));
+    console.log($(this).val());
+    index++;
+  });
+  index = 1;
+  $("[data-minute-index]").each(function () {
+    $(this).val(localStorage.getItem("minute" + index));
+    index++;
+  });
+  index = 1;
+  $("[data-am-pm-index]").each(function () {
+    $(this).val(localStorage.getItem("am-pm" + index));
+    index++;
+  });
+  index = 1;
+  $("[data-text-index]").each(function () {
+    $(this).val(localStorage.getItem("text" + index));
+    console.dir($(this));
+    index++;
+  });
+  index = 1;
+  $("[data-progress-index]").each(function () {
+    $(this).text(localStorage.getItem("progress" + index));
+    index++;
+  });
+};
+getLocalData();
+$(window).click(function () {
+  console.log("click");
+  saveLocalData();
+});
+
 getDate();
 var count = 0;
 timeStatus();
-$("[data-index]").each(function () {
-  count++;
-  console.log($(this).attr("data-index"));
-});
-
+// $("[data-index]").each(function () {
+//   count++;
+//   console.log($(this).attr("data-index"));
+// });
 // checkTime();
